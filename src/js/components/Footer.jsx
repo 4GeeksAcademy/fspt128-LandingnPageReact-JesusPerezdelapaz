13b19ac70739ff72ad81b5944937ac19b8ef55e8
Lgsus23 <jesusbernardops23@gmail.com>
2025-12-17 21:31:58 +0000

    primera revision

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
     return (
-        <footer class="py-3 my-4 bg-black bg-gradient">
+        <footer class="py-3 my-4 bg-black bg-gradient mb-0">
            
-            <p class="text-center text-body-secondary">© 2025 Company, Inc</p>
+            <p class="text-center text-white ">© 2025 Jesús Pérez de la paz</p>
         </footer>
     )
 }
